1ab70ca3bfcd430390372d9f49e95126a2761782
Javier <perezjvr6@gmail.com>
2022-04-12 13:30:24 +0200

    Implementación de la función load_list(list), esta convertirá cada línea del fichero csv en un Customer, y lo añadirá a la lista de objetos customer_list. Con esto obtenemos una gran versatilidad al poder manipular cada elemento del fichero csv como un objeto de la clase, y poder acceder a sus atributos con el punto(.). También he considerado las excepciones en caso de campos requeridos vacíos o filas vacías al completo.

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import csv 
-
-customer_list = []
+from datetime import *
 
 class Customer: 
 	FirstName = ""
@@ -26,4 +25,21 @@ class Customer:
 		self.Phone = p 
 		self.Company = co # required
 
+def load_list(list): # creates a list of Customer objects
+	with open('sample_test.csv', 'r') as csv_reader:
+		reader = csv.reader(csv_reader)
+		headings = next(reader) # store the headers in a separate variable
+		print("Loading csv file...")
+		for row in reader:
+			if not ''.join(row).strip(): # if a row is empty, continue with the loop
+				print("Empty row...")
+				continue
 
+			if len(row[2])==0 or len(row[3])==0 or len(row[4])==0 or len(row[6])==0 or len(row[9])==0: # check if a required field is empty
+				print('A required field is empty in the row: ')
+				print(row)
+				pass 
+			customer_list.append(Customer(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])) # add to the list
+
+customer_list = []
+load_list(customer_list) # now we can manipulate the fields of the csv as objects of the class
